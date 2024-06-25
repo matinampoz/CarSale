03203af4600def6312431e7cecb8625720d171ea
matinampoz <matinampoz@gmail.com>
2024-06-25 23:54:59 +0300

    add class sale

--- ICarSale.java
+++ ICarSale.java
@@ -1,2 +1,17 @@
+/**
+ * Interface representing a car sale.
+ */
+
 public interface ICarSale {
+    /**
+     * Gets the model year of the car.
+     * @return the model year of the car.
+     */
+    public int getModelYear();
+
+    /**
+     * Returns a string representation of the car sale.
+     * @return a string representation of the car sale.
+     */
+    public String toString();
 }
